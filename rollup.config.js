a5ea243fd2486e8b08dd455978cf5d829287205b
shreejitMD <shreejit@metadesignsolutions.co.uk>
2022-09-07 11:24:35 +0545

    Update version

--- rollup.config.js
+++ rollup.config.js
@@ -26,9 +26,4 @@ export default [
       typescript({ tsconfig: "./tsconfig.json" }),
     ],
   },
-  {
-    input: "dist/esm/index.d.ts",
-    output: [{ file: "dist/index.d.ts", format: "esm" }],
-    plugins: [dts()],
-  },
 ];
